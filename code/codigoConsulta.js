2d333e056527f86db9c862ca4057a687a0f6c0ca
HeySeous <jvazquez73@alumnos.uaq.mx>
2023-07-05 13:35:45 -0600

    Rediseño de bloques

---
+++ code/codigoConsulta.js
@@ -0,0 +1,7 @@
+const user = document.getElementById('user');
+user.textContent = sessionStorage.getItem('username');
+
+// function agregarAlumno() {
+//     var divAgregarAlumno = document.getElementById('agregarAlumno');
+//     divAgregarAlumno.style.display = 'block';
+// }
