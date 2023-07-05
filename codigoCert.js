b4d66a5a411d19f1c1f87e87b4887d139822007c
HeySeous <jvazquez73@alumnos.uaq.mx>
2023-07-05 10:34:43 -0600

    Rama nueva

---
+++ codigoCert.js
@@ -0,0 +1,59 @@
+const dataContainer = document.getElementById('tabla');
+const urlParams = new URLSearchParams(window.location.search);
+const id = urlParams.get('id');
+
+const alumno = document.getElementById('alumno');
+alumno.textContent += id;
+
+fetch('http://localhost:3000/api/alumnos/'+id)
+.then(response => response.json())
+.then(data => {
+    alumno.textContent += " "+data[0].nombres;
+    alumno.textContent += " "+data[0].apellidos;
+    // Create the table element
+    const table = document.createElement('table');
+
+    // Create the table header row
+    const headerRow = document.createElement('tr');
+    let contadorFila=1;
+    let contador=1;
+    for (const key in data[0]) {
+        if(contador>8){
+        const th = document.createElement('th');
+        th.textContent = key;
+        headerRow.appendChild(th);
+        } contador++;
+    }
+    table.appendChild(headerRow);
+
+    // Create the table body rows
+    
+    data.forEach(item => {
+    
+    const row = document.createElement('tr');
+    contador=0;
+    for (const key in item) {
+        if(contador>7){
+        const td = document.createElement('td');
+        td.textContent = item[key];
+        
+        row.appendChild(td);
+        } contador++;
+        
+    }
+    //Cambiaq color de fila
+    if(contadorFila%2!=0){
+        row.classList.add('odd-row');
+    }
+    contadorFila++;
+    table.appendChild(row);
+    });
+
+    // Append the table to the container
+    dataContainer.appendChild(table);
+
+})
+.catch(error => {
+console.error(error);
+dataContainer.textContent = 'Error loading data';
+});
---
+++ codigoLogin.js
@@ -0,0 +1,23 @@
+function handleFormSubmission(event) {
+    event.preventDefault();
+    const usuario = document.getElementsByName('user')[0].value;
+    const contrasena = document.getElementsByName('pass')[0].value;
+
+    //Checks if the user exists in the database
+    fetch('http://localhost:3000/api/admins/'+usuario+'/'+contrasena)
+    .then(response => response.json())
+    .then(data => {
+        if(data.length>0){
+            sessionStorage.setItem('username', usuario);
+            sessionStorage.setItem('logged', true);
+            window.location.href = 'consultaAlumnos.html';
+        }else{
+            alert("Usuario o contraseña incorrectos");
+        }
+    })
+    .catch(error => {
+        console.error(error);
+        alert('Error de conexión con la base de datos');
+    });
+
+}
