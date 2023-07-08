b157598f02eb04c8189b36480527281b981b8462
HeySeous <jvazquez73@alumnos.uaq.mx>
2023-07-07 21:30:57 -0600

    Backdrop de cursos, css y js

--- code/codigoConsultaCursos.js
+++ code/codigoConsultaCursos.js
@@ -5,11 +5,6 @@ if (sessionStorage.getItem('logged') == null || sessionStorage.getItem('logged')
 const user = document.getElementById('user');
 user.textContent = sessionStorage.getItem('username');
 
-function agregarCursoVisibilidad() {
-    let divAgregarCurso = document.getElementById('bkdrpFormAgregarCurso');
-    divAgregarCurso.style.display = 'flex';
-}
-
 const dataContainer = document.getElementById('tabla');
 
 fetch('http://localhost:3000/api/certificados')
@@ -48,6 +43,48 @@ fetch('http://localhost:3000/api/certificados')
     })
     .catch(error => {
         // Handle errors here
-        console.error(error);
+        alert('Error de conexión con la base de datos');
     }
     );
+
+    function agregarCursoVisibilidad() {
+        let divAgregarCurso = document.getElementById('bkdrpFormAgregarCurso');
+        divAgregarCurso.style.display = 'flex';
+    }
+    
+    function salir(event) {
+        let form = document.getElementById("bkdrpFormAgregarCurso");
+        form.style.display = 'none';
+    }
+
+    function agregarCurso(event) {
+        event.preventDefault();
+        let nombre = document.getElementsByName('nombre')[0].value;
+        let descripcion = document.getElementsByName('descripcion')[0].value;
+        let logo = document.getElementsByName('logo')[0].value;
+
+        fetch('http://localhost:3000/api/certificados', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                "nombre": nombre,
+                "descripcion": descripcion,
+                "logo": logo
+            })
+        })
+            .then(response => response.json())
+            .then(data => {
+                alert("Curso agregado con éxito");
+                let form = document.getElementById("formAgregarCurso");
+                form.reset();
+                window.location.reload();
+            }
+            )
+            .catch(error => {
+                console.error(error);
+                alert('Error de conexión a la ruta de certificados');
+            }
+            )
+    }
